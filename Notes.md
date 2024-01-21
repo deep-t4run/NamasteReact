f98052197cad659afb9531926b76110b76d8141c
deep-t4run <deep.t4run@gmail.com>
2024-01-21 20:08:14 +0530

    Laying the foundation

--- Notes.md
+++ Notes.md
@@ -4,7 +4,7 @@
 2. Bundling
 3. Minify
 4. Cleaning our code
-5. ev and Production build
+5. Dev and Production build
 6. Image optimization
 7. Caching while development
 8. Compression
@@ -14,3 +14,21 @@
 12. Consistent Hashing Algorithm
 13. Zero config require
 14. Creates a server
+15. Tree Shaking - Removing un-wanted packages
+
+- JSX
+
+1. JSX uses React.createElement() behind the scenes
+2. JSX is converted to JavaScript
+3. JSX -> React.createElement() -> Object -> HTML(DOM)
+4. Babel converts the JSX into javascript
+
+- React Components
+
+1. Functional Components - NEW
+2. Class based Components - OLD
+
+- Functional component: It is a function which returns some peice of react element
+- It is a convention (not necessarily) to write a component name starting with a capital letter
+- We can write any piece of JS code inside {} braces
+- Component Composition : Using component inside a component
--- app.js
+++ app.js
@@ -1,28 +1,44 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const heading1 = React.createElement(
-  "h1",
-  {
-    id: "title",
-  },
-  "Namaste Everyone! from Parcel"
+// const heading1 = React.createElement(
+//   "h1",
+//   {
+//     id: "title",
+//     key: "h1",
+//   },
+//   "Namaste Everyone! from Parcel"
+// );
+
+const heading = (
+  <h1 id="title1" className="h1" key="h1">
+    Namaste React! Welcome from heading element
+  </h1>
 );
 
-const heading2 = React.createElement(
-  "h2",
-  {
-    id: "greeting",
-  },
-  "Welcome to Namaste React!"
-);
+const Title = () => {
+  return (
+    <h1 id="title" className="heading" key="heading">
+      Welcome from Title component
+    </h1>
+  );
+};
 
-const container = React.createElement(
-  "div",
-  {
-    id: "container",
-  },
-  [heading1, heading2]
-);
+// React Components
+// functional component
+const HeaderComponent = () => {
+  return (
+    <div>
+      {heading}
+      {/* {Title()} */}
+      <Title />
+      <h1>Namaste React : Functional Component</h1>
+      <h2>Welcome from HeaderComponent</h2>
+      {console.log("Write any JS code")}
+    </div>
+  );
+};
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(container);
+// root.render(heading);
+root.render(<HeaderComponent />);
